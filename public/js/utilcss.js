7f3b39a98a9322fff54400acf4a4fe44ecbb5ec5
Javier <jegj57@gmail.com>
2017-03-06 13:25:08 -0500

    Add underscore to dependicies

--- public/js/utilcss.js
+++ public/js/utilcss.js
@@ -1,25 +1,24 @@
 var getVariable = function(styles, propertyName) {
-  return String(styles.getPropertyValue(propertyName)).trim();
+	return String(styles.getPropertyValue(propertyName)).trim();
 };
 
 var setDocumentVariable = function(propertyName, value) {
-  document.documentElement.style.setProperty(propertyName, value);
+	document.documentElement.style.setProperty(propertyName, value);
 };
 
-
-var createTemplate = function(html,css){
-  return html.replace("<style></style>", `<style  is="custom-style" include="layout-style">${css}</style>`);
+var createTemplate = function(html, css ){
+	return html.replace("<style></style>", `<style	is="custom-style" include="layout-style">${css}</style>`);
 }
 
 var createTemplate2 = function(html,css){
-  var template=html
- if(css){
-   template= `<custom-style>
-                <style>
-                  ${css}
-                </style>
-              </custom-style>
-              ${html}`;
- }
- return template;
+	var template=html
+	if(css){
+		template= `<custom-style>
+								<style>
+									${css}
+								</style>
+							</custom-style>
+							${html}`;
+	}
+	return template;
 }
