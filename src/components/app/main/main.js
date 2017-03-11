3424a42a48c58f05d2817d035ceaa394e7dcede0
Edinson Nuñez More <nmedinson@gmail.com>
2017-03-11 14:23:17 -0500

    add footer animation

--- src/components/app/main/main.js
+++ src/components/app/main/main.js
@@ -16,19 +16,49 @@ export default class AppMain extends HTMLElement{
 			this.$chips = this.shadowRoot.querySelector('app-carousel-chips');
 			this.$footer = this.shadowRoot.querySelector('.footer-container');
 			this.$footerTitle = this.shadowRoot.querySelector('.footer-title');
+			this.$footerLogo = this.shadowRoot.querySelector('.footer-logo');
 			this.$footerDescription = this.shadowRoot.querySelector('.footer-description');
+		}
+		animatianimateFooter(element,...params){
+
+			for (let i = 0, length = params.length; i < length; i++) {
+				element.classList.add(params[i]);
+			}
+			element.addEventListener('animationend',()=>{
+				for (let i = 0, length = params.length; i < length; i++) {
+					element.classList.remove(params[i]);
+				}
+
+			});
 		}
 		addListerners(){
 			this.$chips.addEventListener('changed-chip',(e)=>{
 				console.log("changed-chip",e);
 				if(e.detail.active){
 					this.$footer.classList.remove('hidden');
+					/*this.$footerLogo.classList.add('animated','zoomIn');
+					setTimeout(()=>{
+						this.$footerLogo.classList.remove('animated','zoomIn');
+					},1000);*/
+					animate(this.$footerLogo,'animated','zoomIn');
+
 				}else{
 					this.$footer.classList.add('hidden');
+					/*this.$footerLogo.classList.add('animated','zoomOut');
+					setTimeout(()=>{
+						this.$footerLogo.classList.remove('animated','zoomOut');
+					},1000);
+					*/
+					animate(this.$footerLogo,'animated','zoomOut');
 				}
 				this.footerTitle=e.detail.data.title;
 				this.footerDescription=e.detail.data.description;
-				this.render()
+				
+				this.render();
+			});
+
+			this.$footerLogo.addEventListener('click',()=>{
+				alert("Hola Sebas..el rey de los videojuegos XD");
 			});
 		}
 
@@ -39,6 +69,7 @@ export default class AppMain extends HTMLElement{
 		render(){
 			this.$footerTitle.innerHTML=this.footerTitle;
 			this.$footerDescription.innerHTML=this.footerDescription;
+			
 		}
 
 }
