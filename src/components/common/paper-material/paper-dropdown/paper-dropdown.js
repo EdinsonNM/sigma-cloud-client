3f7eeaf60646e9fdc0e9eefdba56c58916d6b06e
Javier <jegj57@gmail.com>
2017-03-15 11:22:55 -0500

    Add style for paper-dropdown

--- src/components/common/paper-material/paper-dropdown/paper-dropdown.js
+++ src/components/common/paper-material/paper-dropdown/paper-dropdown.js
@@ -13,18 +13,50 @@ export default class PaperDropDown extends HTMLElement {
 		shadowRoot.innerHTML = templateObj.template;
 		this.initDOMRefs();
 		this.collectDataAttributes();
+		this.addListeners();
+		this.active = false;
 	}
 
 	collectDataAttributes(){
+		if ( this.attributes['bg-color'] ) {
+			this.bgColor = this.attributes['bg-color'].nodeValue;
+		}
 
+		if ( this.attributes['label'] ) {
+			this.label = this.attributes['label'].nodeValue;
+		}
+	}
+
+	addListeners(){
+		this.$button.addEventListener("click",(e)=>{
+			if ( this.active ) {
+				this.active = false;
+				this.$list.classList.remove("mui--is-open");
+			} else {
+				this.active = true;
+				this.$list.className += " mui--is-open"
+			}
+		})
 	}
 
 	initDOMRefs(){
-
+		this.$button = this.shadowRoot.querySelector("#button");
+		this.$list = this.shadowRoot.querySelector("#list");
 	}
 
-
 	connectedCallback(){
+		if ( this.bgColor ) {
+			this.$button.style = "background-color: " + this.bgColor;
+		}
+
+		if ( this.label ) {
+			let span = document.createElement('span');
+			let text = document.createTextNode(this.label+ ' ');
+			span.setAttribute('class', 'mui-caret');
+			this.$button.innerHTML = "";
+			this.$button.appendChild(text);
+			this.$button.appendChild(span);
+		}
 
 	}
 
