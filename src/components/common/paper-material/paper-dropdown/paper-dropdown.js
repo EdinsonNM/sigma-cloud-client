4ee2586acd1df46782aa32acb62c30189a19ba7d
Javier <jegj57@gmail.com>
2017-03-20 10:53:16 -0500

    Add custom style for new dropdown

--- src/components/common/paper-material/paper-dropdown/paper-dropdown.js
+++ src/components/common/paper-material/paper-dropdown/paper-dropdown.js
@@ -18,56 +18,73 @@ export default class PaperDropDown extends HTMLElement {
 	}
 
 	collectDataAttributes(){
-		if ( this.attributes['bg-color'] ) {
-			this.bgColor = this.attributes['bg-color'].nodeValue;
-		}
-
+		// if ( this.attributes['bg-color'] ) {
+		// 	this.bgColor = this.attributes['bg-color'].nodeValue;
+		// }
+		//
+		//
 		if ( this.attributes['label'] ) {
 			this.label = this.attributes['label'].nodeValue;
+		}
+
+		if ( this.attributes['default-value'] ) {
+			this.defaultValue = this.attributes['default-value'].nodeValue;
 		}
 	}
 
 	addListeners(){
 		const self = this;
-		this.$button.addEventListener("click",(e)=>{
+
+		this.$list.addEventListener('animationend', ()=>{
 			if ( this.active ) {
 				this.active = false;
-				this.$list.classList.remove("mui--is-open");
-			} else {
+				this.$list.classList.remove('animated', 'fadeOut');
+				this.$list.style.display = 'none';
+			} else  {
 				this.active = true;
-				this.$list.className += " mui--is-open"
+				this.$list.classList.remove('animated', 'fadeIn');
+				this.$list.style.display = '';
 			}
-		})
-
-		document.addEventListener('dropOptionChanged', (e)=> {
-			self.	changeLabel(e.detail.label);
-			self.active = false;
-			self.$list.classList.remove("mui--is-open");
 		});
+
+		this.$button.addEventListener("click",(e)=>{
+			if ( this.active ) {
+				this.$list.classList.add('animated', 'fadeOut');
+			} else {
+				this.$list.style.display = '';
+				this.$list.classList.add('animated', 'fadeIn');
+			}
+		});
+
+		// document.addEventListener('dropOptionChanged', (e)=> {
+		// 	self.	changeLabel(e.detail.label);
+		// 	self.active = false;
+		// 	self.$list.classList.remove("mui--is-open");
+		// });
 	}
 
 	initDOMRefs(){
 		this.$button = this.shadowRoot.querySelector("#button");
 		this.$list = this.shadowRoot.querySelector("#list");
+		this.$label = this.shadowRoot.querySelector("#label");
+		this.$value = this.shadowRoot.querySelector("#value-container");
 	}
 
 	changeLabel(label) {
 		if ( label ) {
-			let span = document.createElement('span');
-			let text = document.createTextNode(label + ' ');
-			span.setAttribute('class', 'mui-caret');
-			this.$button.innerHTML = "";
-			this.$button.appendChild(text);
-			this.$button.appendChild(span);
+			this.$label.innerText = label;
+		}
+	}
+
+	changeValue(value) {
+		if ( value ) {
+			this.$value.innerText = value;
 		}
 	}
 
 	connectedCallback(){
-		if ( this.bgColor ) {
-			this.$button.style = "background-color: " + this.bgColor;
-		}
-
 		this.changeLabel(this.label);
+		this.changeValue(this.defaultValue);
 	}
 
 }
